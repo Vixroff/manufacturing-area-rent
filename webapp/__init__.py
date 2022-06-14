de98c198d708a511960abdb273e3a4bf99e02333
Maksim Vikhrov <maxvihr@yandex.ru>
2022-06-14 13:24:57 +0300

    change to flask-sqlalchemy, add map and points

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,9 +1,15 @@
-from flask import Flask
 from config import Config
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
 
 from webapp import routes
---
+++ webapp/static/js/script.js
@@ -0,0 +1,8 @@
+ymaps.ready(init)
+
+function init(){
+    let map = new ymaps.Map('map', {
+        center: [55.06771639944237,60.09706398026949],
+        zoom: [17]
+    });
+}
